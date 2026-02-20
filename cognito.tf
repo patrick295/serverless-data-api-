a084aae51640e5c2c7242c7554c035ab1cc951dc
Patrick <82440798+patrick295@users.noreply.github.com>
2026-02-20 12:08:07 +0100

    Create cognito.tf

---
+++ cognito.tf
@@ -0,0 +1,8 @@
+resource "aws_cognito_user_pool" "user_pool" {
+  name = "${var.project_name}-user-pool"
+}
+
+resource "aws_cognito_user_pool_client" "client" {
+  name         = "api-client"
+  user_pool_id = aws_cognito_user_pool.user_pool.id
+}
